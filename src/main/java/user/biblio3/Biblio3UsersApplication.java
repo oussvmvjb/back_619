49a94e92b083b49fe3896c85b96050514d2b62a9
oussvmv jabri <oussvmv@example.com>
2025-11-02 23:07:32 +0100

    Ajout du code backend (API utilisateurs + Auth)

---
+++ src/main/java/user/biblio3/Biblio3UsersApplication.java
@@ -0,0 +1,12 @@
+package user.biblio3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biblio3UsersApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Biblio3UsersApplication.class, args);
+    }
+}
---
+++ src/test/java/user/biblio3/Biblio3UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package user.biblio3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biblio3UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
